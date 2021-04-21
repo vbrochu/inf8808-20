268379b0484b26b3d3aa085365e35ccf85a3ddae
vbrochu <vincent.brochu@polymtl.ca>
2021-04-21 18:45:35 -0400

    Create groups.js

---
+++ groups.js
@@ -0,0 +1,166 @@
+/*
+#00876c
+#2c9470
+#45a074
+#5cad78
+#72b97c
+#88c580
+#9fd184
+#b6dd89
+#cee98f
+#e6f495
+#ffff9d
+#ffed8a
+#fedb79
+#fcc86b
+#fab560
+#f7a258
+#f38f52
+#ed7b4f
+#e7674e
+#de534f
+#d43d51
+*/
+
+export const colors = {
+    'Acteur':'#8fda59',
+    'Production':'#42952e',
+    'Technique':'#b3b3b3',
+    'Esthetique':'#bfcd8e',
+    'Image':'#4cf32c',
+    'Son':'#eb46ea',
+    'Scénario':'#713d83',
+    'Organisme':'#8115b4',
+    'Narration':'#e9d737',
+    'Documentaire/émission':'#b3b3b3',
+    'Science':'#b3b3b3',
+    'Régie':'#b3b3b3',
+    'Réalisation':'#35618f',
+    'Direction artistique':'#9e7ee9',
+    'Direction de production':'#34debb',
+    'Photographe de plateau':'#da2d82',
+    'Générique additionnel':'#828282',
+    'Maison de services':'#b3b3b3',
+    'Conception':'#b3b3b3',
+    'Création':'#b3b3b3'
+}
+
+export const groups = {
+    24: 'Acteur',
+    25: 'Acteur',
+    15: 'Acteur',
+
+    28: 'Production',
+    29: 'Production',
+    48: 'Production',
+
+    4: 'Technique',
+    5: 'Technique',
+
+    6: 'Esthetique',
+    8: 'Esthetique',
+    9: 'Esthetique',
+    17: 'Esthetique',
+
+    2: 'Image',
+    12: 'Image',
+    14: 'Image',
+    19: 'Image',
+
+    7: 'Son',
+    18: 'Son',
+    20: 'Son',
+    27: 'Son',
+    16: 'Son',
+    21: 'Son',
+
+    31: 'Scénario',
+    32: 'Scénario',
+    33: 'Scénario',
+
+    34: 'Organisme',
+    39: 'Organisme',
+    40: 'Organisme',
+    43: 'Organisme',
+    49: 'Organisme',
+    37: 'Organisme',
+
+    22: 'Narration',
+    23: 'Narration',
+
+    35: 'Documentaire/émission',
+    36: 'Documentaire/émission',
+    47: 'Documentaire/émission',
+
+    38: 'Science',
+    41: 'Science',
+
+    30: 'Régie',
+
+    3: 'Réalisation',
+    1: 'Réalisation',
+
+    10: 'Direction artistique',
+
+    11: 'Direction de production',
+
+    26: 'Photographe de plateau',
+
+    13: 'Générique additionnel',
+
+    42: 'Maison de services',
+
+    44: 'Conception',
+
+    45: 'Création'
+}
+
+export const roles = {
+    1:"Réalisation",
+    15:"Interprétation",
+    34:"Société de production",
+    28:"Producteur",
+    14:"Images",
+    31:"Scénario",
+    37:"Financement",
+    24:"Participant",
+    19:"Montage images",
+    21:"Musique",
+    27:"Prise de son",
+    43:"Télédiffuseur",
+    13:"Générique additionnel",
+    2:"Animation",
+    29:"Producteur délégué",
+    38:"Recherche",
+    39:"Société de distribution",
+    10:"Direction artistique",
+    20:"Montage sonore",
+    23:"Narrateur",
+    35:"Animateur d'émission",
+    18:"Mixage",
+    25:"Personnage",
+    26:"Photographe de plateau",
+    16:"Interprète musique",
+    11:"Direction de production",
+    48:"Producteur exécutif",
+    8:"Costumes",
+    7:"Conception sonore",
+    32:"Scripte",
+    22:"Narration",
+    3:"Assistant réalisation",
+    17:"Maquillage",
+    9:"Décors",
+    36:"Journaliste",
+    44:"Conception",
+    47:"Chroniqueur",
+    6:"Coiffure",
+    12:"Effets spéciaux",
+    30:"Régie",
+    40:"Société d'exportation",
+    5:"Chef machiniste",
+    4:"Chef électricien",
+    45:"Création",
+    49:"Diffuseur Web",
+    42:"Maison de services",
+    41:"Laboratoire"
+}
